2925d0a5a55c0c559a1688630aec4e07f12f7e4c
KarmaPol <kkhkr98@gmail.com>
2024-06-24 23:58:35 +0900

    feat: 멀티스레드로 여러 페이지 한번에 크롤링하도록 수정

---
+++ src/coupang_review_crawler.py
@@ -0,0 +1,131 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from bs4 import BeautifulSoup as bs
+from typing import Optional,Union,Dict,List
+import time
+import os
+import re
+import requests as rq
+import json
+
+def get_headers(
+        key: str,
+        default_value: Optional[str] = None
+)-> Dict[str,Dict[str,str]]:
+    """ Get Headers """
+    JSON_FILE : str = '../json/headers.json'
+
+    with open(JSON_FILE,'r',encoding='UTF-8') as file:
+        headers : Dict[str,Dict[str,str]] = json.loads(file.read())
+
+    try :
+        return headers[key]
+    except:
+        if default_value:
+            return default_value
+        raise EnvironmentError(f'Set the {key}')
+
+class Coupang:
+    @staticmethod
+    def get_product_code(url: str)-> str:
+        """ 입력받은 URL 주소의 PRODUCT CODE 추출하는 메소드 """
+        prod_code : str = url.split('products/')[-1].split('?')[0]
+        return prod_code
+
+    def __init__(self)-> None:
+        self.__headers : Dict[str,str] = get_headers(key='headers')
+
+    def main(self, item_url, page_count)-> List[List[Dict[str,Union[str,int]]]]:
+        # URL 주소
+        URL : str = item_url # 컨트롤러에서 URL 주소 입력받기
+
+        # URL의 Product Code 추출
+        prod_code : str = self.get_product_code(url=URL)
+
+        # URL 주소 재가공
+        URLS : List[str] = [f'https://www.coupang.com/vp/product/reviews?productId={prod_code}&page={page}&size=5&sortBy=ORDER_SCORE_ASC&ratings=&q=&viRoleCode=3&ratingSummary=true' for page in range(1,page_count + 1)] # 컨트롤러에서 페이지 수 입력받기
+
+        # __headers에 referer 키 추가
+        self.__headers['referer'] = URL
+        
+        crawl_data : List[List[Dict[str,Union[str,int]]]] = list()
+
+        with rq.Session() as session:
+            with ThreadPoolExecutor(max_workers=20) as executor:
+                future_to_url = {executor.submit(self.fetch, url, session): url for url in URLS}
+                for future in as_completed(future_to_url):
+                    try:
+                        data = future.result()
+                        if data:
+                            crawl_data.append(data)
+                    except Exception as e:
+                        raise e
+
+        return crawl_data
+
+    def fetch(self,url:str,session)-> List[Dict[str,Union[str,int]]]:
+        save_data : List[Dict[str,Union[str,int]]] = list()
+
+        with session.get(url=url,headers=self.__headers) as response :
+            html = response.text
+            soup = bs(html,'html.parser')
+
+            # Article Boxes
+            article_lenth = len(soup.select('article.sdp-review__article__list'))
+
+            for idx in range(article_lenth):
+                dict_data : Dict[str,Union[str,int]] = dict()
+                articles = soup.select('article.sdp-review__article__list')
+
+                # 구매자 이름
+                user_name = articles[idx].select_one('span.sdp-review__article__list__info__user__name')
+                if user_name == None or user_name.text == '':
+                    user_name = '-'
+                else:
+                    user_name = user_name.text.strip()
+
+                # 평점
+                rating = articles[idx].select_one('div.sdp-review__article__list__info__product-info__star-orange')
+                if rating == None:
+                    rating = 0
+                else :
+                    rating = int(rating.attrs['data-rating'])
+
+                # 구매자 상품명
+                prod_name = articles[idx].select_one('div.sdp-review__article__list__info__product-info__name')
+                if prod_name == None or prod_name.text == '':
+                    prod_name = '-'
+                else:
+                    prod_name = prod_name.text.strip()
+
+                # 헤드라인(타이틀)
+                headline = articles[idx].select_one('div.sdp-review__article__list__headline')
+                if headline == None or headline.text == '':
+                    headline = '-'
+                else:
+                    headline = headline.text.strip()
+
+                # 리뷰 내용
+                review_content = articles[idx].select_one('div.sdp-review__article__list__review > div')
+                if review_content == None :
+                    review_content = '-'
+                else:
+                    review_content = re.sub('[\n\t]','',review_content.text.strip())
+
+                # 맛 만족도
+                answer = articles[idx].select_one('span.sdp-review__article__list__survey__row__answer')
+                if answer == None or answer.text == '':
+                    answer = '-'
+                else:
+                    answer = answer.text.strip()
+
+                dict_data['prod_name'] = prod_name
+                dict_data['user_name'] = user_name
+                dict_data['rating'] = rating
+                dict_data['headline'] = headline
+                dict_data['review_content'] = review_content
+                dict_data['answer'] = answer
+
+                save_data.append(dict_data)
+
+            return save_data
